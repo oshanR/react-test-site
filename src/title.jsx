ef982030d9ae1c51f78ac91d43f1b3d2893675cc
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-09-06 01:55:44 +0530

    Update title.jsx

--- src/title.jsx
+++ src/title.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
     return (
-        <div class="topnav" id="myTopnav">
+        <div className="topnav" id="myTopnav">
             <a href="#home" class="active">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
