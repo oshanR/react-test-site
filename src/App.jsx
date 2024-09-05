7cbb80d16a5e03356d234fcfd9c1ced41128c391
Oshan Rashmitha <oshanrashmitha416@gmail.com>
2024-09-06 01:51:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,11 @@ import './App.css'
 import Title from './title'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <Title />
-      </div>
-      <h1>Oshan humtha</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          No of times molested: {count}
-        </button>
-      </div>
-    </>
+    
+      <Title />
+
   )
 }
 
