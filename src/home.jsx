885fb96eb89a06627b0d347f670ca7b55cbe3d93
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-09-06 01:55:39 +0530

    Create home.jsx

---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            <p>This is the content of the home page.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, nunc id aliquam tincidunt, nisl nunc lacinia nunc, id lacinia nunc justo id nunc. Sed euismod, nunc et aliquet ultrices, nunc nunc aliquam justo, id aliquam justo nunc id justo. Sed euismod, nunc et aliquet ultrices, nunc nunc aliquam justo, id aliquam justo nunc id justo.</p>
+        </div>
+    );
+};
+
+export default Home;
